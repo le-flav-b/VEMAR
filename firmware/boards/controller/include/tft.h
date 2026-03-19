253841e02ee584d4e6cf7255884b582c8ad97d60
Christian Hugon <chriss.hugon@gmail.com>
2026-03-19 14:50:42 +0100

    update docs for tft

--- firmware/boards/controller/include/tft.h
+++ firmware/boards/controller/include/tft.h
@@ -2,20 +2,10 @@
 #define VEMAR_TFT_H
 
 #include "ili9341.h"
-#include "color.h"
 
 #if !defined(DEFINE_TFT_ILI9341)
 #error Module 'ILI9341' not defined
 #endif
-
-// #define RGB565_WHITE 0xFFFF
-// #define RGB565_YELLOW 0xFFE0
-// #define RGB565_MAGENTA 0xF81F
-// #define RGB565_RED 0xF800
-// #define RGB565_CYAN 0x07FF
-// #define RGB565_GREEN 0x07E0
-// #define RGB565_BLUE 0x001F
-// #define RGB565_BLACK 0x0000
 
 /**
  * @brief Define the orientation of the display
@@ -40,13 +30,16 @@ typedef enum
 
 } tft_dir_t;
 
+/**
+ * @brief Define text size
+ */
 typedef enum
 {
-	TFT_TEXT_XS = 1,
-	TFT_TEXT_S = 2,
-	TFT_TEXT_M = 3,
-	TFT_TEXT_L = 4,
-	TFT_TEXT_XL = 5
+	TFT_TEXT_XS = 1, /**< Extra small (5x7) */
+	TFT_TEXT_S = 2,	 /**< Small (10x14) */
+	TFT_TEXT_M = 3,	 /**< Medium (15x21) */
+	TFT_TEXT_L = 4,	 /**< Large (20x28) */
+	TFT_TEXT_XL = 5	 /**< Extra Large (25x35) */
 } tft_text_t;
 
 /**
@@ -65,26 +58,48 @@ void TFT_init(pin_t cs, pin_t dc, pin_t rst);
  */
 void TFT_set_mode(tft_mode_t orientation, tft_dir_t x, tft_dir_t y);
 
-void TFT_setup_text(tft_text_t size, length_t spacing, color16_t color, color16_t background);
+/**
+ * @brief Modify text configuration
+ * @param size Size of the text
+ * @param spacing Spacing between characters
+ * @param color Color of the text
+ * @param background Text background
+ * @see tft_text_t
+ */
+void TFT_setup_text(tft_text_t size,
+					length_t spacing,
+					color16_t color,
+					color16_t background);
 
 /**
  * @brief Fill the screen with the specific color
- * @param color Color to fill the screen
+ * @param color Color to fill the screen with
  */
 inline void TFT_fill_screen(color16_t color)
 {
 	ILI9341_fill_screen(color);
 }
 
+/**
+ * @brief Display a character
+ * @param x Position X of the character (Top-Left)
+ * @param y Position Y of the character (Top-Left)
+ * @param ch Character to display
+ */
 inline void TFT_print_char(uint16_t x, uint16_t y, char ch)
 {
 	ILI9341_draw_char(x, y, ch);
 }
 
+/**
+ * @brief Display a text
+ * @param x Position X of the text (Top-Left)
+ * @param y Position Y of the text (Top-Left)
+ * @param str Null-terminated string
+ */
 inline void TFT_print_str(uint16_t x, uint16_t y, const char *str)
 {
 	ILI9341_draw_string(x, y, str);
 }
-
 
 #endif // VEMAR_TFT_H
